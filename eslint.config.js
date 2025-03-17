73d4f863c8334faa025797a1b57763f0256b0b07
Abigail-Diaz <abigailsolano128@gmail.com>
2025-03-16 22:55:39 -0700

    On branch week-01-setup  Your branch is up to date with 'origin/week-01-setup'.  Changes to be committed: Updated rules 	modified:   eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -23,11 +23,13 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+         //this suppresses warnings about not usin
       ],
+      'react/prop-types': 'off',
     },
   },
 ]
