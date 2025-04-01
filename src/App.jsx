5be572c09cf91c72f57789ea00ddc2b7fac8ff3b
Abigail-Diaz <abigailsolano128@gmail.com>
2025-04-01 12:06:21 -0700

    On branch week-03-props Changes to be committed:
        feat(todo): Create TodoListItem component and update todos rendering to use component-based approach.
    
        - Added `TodoListItem.jsx` component that accepts a `todo` prop and renders a `<li>` element with the
            `todo.title` value.
        - Updated `App.jsx`:
            - Imported `useState` hook and initialized `newTodo` state with `newTodo` and `setNewTodo` variables.
            - Set initial state value of `newTodo` to short string `'add todos here'`.
            - Placed `<p>{newTodo}</p>` between `<TodoForm/>` and `<TodoList/>`.
                self-closing component tags.
        - Refactored `TodoList.jsx` to render `TodoListItem` components for each todo in `todos` array:
            - Imported `TodoListItem` component from `./TodoListItem.jsx`.
            - Updated `TodoList` to map over `todos` array and render a `TodoListItem` component for each todo.
            - Passed `todo` as a prop to `TodoListItem` instance inside `.map()`.
            - Passed `key = {todo.id}` to `TodoListItem` for React to keep track of each item.
    
    	modified:   src/App.jsx
    	modified:   src/TodoList.jsx
    	new file:   src/TodoListItem.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import './App.css';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
+// import useState hook to create a new state variable
+import { useState } from 'react';
+
 function App() {
-  
+  const [newTodo, setNewTodo] = useState('add todos here');
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm/>
-      <TodoList/>
+      <TodoForm />
+      <p>{newTodo}</p>
+      <TodoList />
     </div>
   );
 }
