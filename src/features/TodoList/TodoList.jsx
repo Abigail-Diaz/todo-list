06a3d7343ec18f00511f1c15198548b3eff15415
Abigail-Diaz <abigailsolano128@gmail.com>
2025-05-20 23:23:32 -0700

    feat: add CSS modules for class-based styling in App, TodoList and TodoListItem - Centered app layout using flex - Added a red border to the error message container - Removed padding and list item bullets - Added bottom padding to the list items

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,5 +1,6 @@
 // import TodoListItem component
 import TodoListItem from './TodoListItem';
+import styles from './TodoList.module.css';
 // function to return a list of todos
 // This component will be used to display the list of todos
 function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
@@ -14,7 +15,7 @@ function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
       {filteredTodoList.length === 0 ? (
         <p>Add todo above to get started</p>
       ) : (
-        <ul>
+        <ul className={styles.list}>
           {/* Render each todos item as a TodoListItem component for better organization*/}
           {filteredTodoList.map((todo) => (
             <TodoListItem
